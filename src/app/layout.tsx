56abde6bce15124091902cabc27103d9ccbf95d7
Emir Aydın <84669393+ibrahimemiraydin@users.noreply.github.com>
2025-02-15 20:59:41 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Soru-Cevap Yarışması",
-  description: "Soru-Cevap Yarışmasına Hoşgeldiniz. Soruları yanıtlayın ve sınavı bitirin. Daha sonra cevaplarınızı kontrol edebilirsiniz.",
+  description: "Soruları yanıtlayın ve sınavı bitirin. Daha sonra yanıtları göster seçeneği ile cevaplarınızı kontrol edebilirsiniz.",
 };
 
 export default function RootLayout({
